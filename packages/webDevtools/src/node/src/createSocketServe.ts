1bfa9850b4ebd9264ba2779791b560af360c5a58
haoming <435203093@qq.com>
2021-08-08 14:20:02 +0800

    feat: createSocketServe

--- packages/webDevtools/src/node/src/createSocketServe.ts
+++ packages/webDevtools/src/node/src/createSocketServe.ts
@@ -1,3 +1,55 @@
-const createSocketServe = () => {}
+import { Server } from 'ws'
+import type { ServerOptions } from 'ws'
+import { HMRPayload } from './types/hmrPayload'
 
-export default createSocketServe
+export function createSocketServe() {
+  const websocketServerOptions: ServerOptions = {
+    port: 24686
+  }
+
+  let bufferedError = null
+
+  const wss = new Server(websocketServerOptions)
+
+  wss.on('connection', (socket) => {
+    socket.send(JSON.stringify({ type: 'connected' }))
+    if (bufferedError) {
+      socket.send(JSON.stringify(bufferedError))
+      bufferedError = null
+    }
+  })
+  wss.on('error', (e: Error & { code: string }) => {
+    if (e.code !== 'EADDRINUSE') {
+      //   config.logger.error(
+      //     chalk.red(`WebSocket server error:\n${e.stack || e.message}`),
+      //     { error: e }
+      //   )
+    }
+  })
+
+  return {
+    send(payload: HMRPayload) {
+      if (payload.type === 'error' && !wss.clients.size) {
+        bufferedError = payload
+        return
+      }
+      const stringified = JSON.stringify(payload)
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(stringified)
+        }
+      })
+    },
+    close() {
+      return new Promise<void>((resolve, reject) => {
+        wss.close((err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      })
+    }
+  }
+}
--- packages/webDevtools/src/node/src/httpProxy.ts
+++ packages/webDevtools/src/node/src/httpProxy.ts
@@ -1,3 +1,17 @@
-const httpProxy = () => {}
+import type { ProxyOptions } from 'vite'
 
-export default httpProxy
+export function httpProxy(
+  options: Record<string, string | ProxyOptions>
+): Record<string, string | ProxyOptions> {
+  // const ProxyOptions  = {};
+
+  //   Object.entries(options).forEach(([key,value],idx)=>{
+
+  //   })
+
+  return {
+    // onProxyRes(app, server, compiler) {},
+    '/': {},
+    ...options
+  }
+}
