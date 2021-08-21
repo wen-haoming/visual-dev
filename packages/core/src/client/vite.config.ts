dff29b0391c76d6ca7f09e5ea55c35639e7dce55
haoming <435203093@qq.com>
2021-08-22 01:27:13 +0800

    fix:  export DEV_SERVER_PORT

--- packages/core/src/client/vite.config.ts
+++ packages/core/src/client/vite.config.ts
@@ -3,6 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import polyfillNode from 'rollup-plugin-polyfill-node'
 
 export const ASSETS_DIR = '_dev-plugin'
+export const DEV_SERVER_PORT = 10010
 
 // https://vitejs.dev/config/
 export default defineConfig({
--- packages/core/src/node/index.ts
+++ packages/core/src/node/index.ts
@@ -4,7 +4,5 @@ export { httpProxy } from './src/httpProxy'
 export { createSocketServe } from './src/createSocketServe'
 export { createServer } from './src/createServer'
 export { insertJSXElementPathPlugin, insertParametersPlugin } from './src/babel'
-export { ASSETS_DIR } from '../client/vite.config'
+export { ASSETS_DIR, DEV_SERVER_PORT } from '../client/vite.config'
 export const CLIENT_ASSETS_DIR = path.resolve(__dirname, '../client')
-
-export const DEV_SERVER_PORT = 10010
