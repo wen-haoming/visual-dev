b6db87f7e041d150dffde4e012a1c2d10ea6dd37
haoming <435203093@qq.com>
2021-08-30 23:53:33 +0800

    fix: tsconfig

--- packages/core/src/createServer.ts
+++ packages/core/src/createServer.ts
@@ -1,21 +1,18 @@
-import * as express from "express";
-import * as cors  from 'cors'
-import * as bodyParser  from 'body-parser'
-import * as launchEditor from '@umijs/launch-editor'
+import express from 'express'
+import cors from 'cors'
+import launchEditor from '@umijs/launch-editor'
 import { SERVER_PORT } from './index'
 
 export const createServer = () => {
   const app = express()
 
   app.use(cors())
-  app.use(bodyParser.json())
 
-  app.post('/launchEditor',async (req,res)=>{
-      const {filePath}  = req.body
-        await launchEditor(process.cwd()+ filePath)
-      res.send('ok')
+  app.post('/launchEditor', async (req, res) => {
+    const { filePath } = req.body
+    await launchEditor(process.cwd() + filePath)
+    res.send('ok')
   })
-
 
   app.listen(SERVER_PORT, () => {
     console.log(`dev-plugin is listening ${SERVER_PORT}`)
