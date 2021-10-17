9fda47c77feb66762a37ef3d702d31c039612917
haoming <435203093@qq.com>
2021-10-18 00:41:28 +0800

    release: visual@0.0.9

--- packages/dev-ui/src/Pages/Setting/index.vue
+++ packages/dev-ui/src/Pages/Setting/index.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { useHotkeys } from '../../hooks';
-
-onMounted(() => {
-  console.log('onMounted');
-});
-
+// import { onMounted } from 'vue';
+// import { useHotkeys } from '../../hooks';
 // useHotkeys('command+b', () => {
 //   alert('hhh');
 // });
 </script>
 <template>
   <div class="l-setting">
-    123
     <!-- <div class="keyboard-wrap"><input type="text" /> + <input type="text" /></div> -->
   </div>
 </template>
